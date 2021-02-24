a6dc265d60623bba9e9773d695d73ccf0417f766
Radman Lotfiazar <57597721+radmanlo@users.noreply.github.com>
2021-02-24 22:49:24 +0300

    paragraph3 done

--- isp.c
+++ isp.c
@@ -7,23 +7,22 @@ int main()
 {
 	printf("WELCOME TO RADMAN'S SHELL \n");
 	int lop = 0;
- 	char command[20] = "";
- 	char cmd[] = "";
-	char argv[20][20];
+ 	char command[20];
+ 	char temp[20]; 
+ 	char *cmd = malloc(20*sizeof(char));
+	char *argv[20];
+	for (int i = 0 ; i<20; i++){
+		argv[i]= malloc(20*sizeof(char));
+	}
 	int argSize;
 	while (lop == 0){
-		//argSize = 0;
+		argSize = 0;
 		printf("isp$ ");
     		scanf("%[^\n]%*c", command); 
-    		printf("Your command: ");
-    		printf("%s \n", command);
-    		printf("cmd: %s \n", cmd);
-    		strncat(argv[0], &command, 1); 
-    		printf("%s \n", argv[0]);
-    		for (int i = 0; command[i] != 0; i++){
+    		for (int i = 0; command[i] != '\0'; i++){
     			if (i == 0 && (command[i] == ' ' || command[i] != ' ' )){
     				if (command[i] != ' '){
-    					while (command[i] != ' '){
+    					while (command[i] != '\0' && command[i] != ' '){
     						strncat(cmd, &command[i], 1); 
     						i++;
     					}
@@ -31,23 +30,56 @@ int main()
     				else{
     					while (command[i] == ' ')
     						i++;
-    					while (command[i] != 0){
+    					while (command[i] != '\0' && command[i] != ' '){
     						strncat(cmd, &command[i], 1); 
     						i++;
     					}
     				}
-    			}
-    			else if (command[i] == ' '){
+    				strcpy(*(argv + argSize), cmd);
     				argSize++;
     			}
+    			else if (command[i] != ' '){
+    				strncat(temp, &command[i], 1); 
+    				strcpy(*(argv + argSize), temp);
+    			}
     			else{
-    				strncat(argv[argSize], &command[i], 1); 
+    				argSize++;
+    				bzero(temp, 20);
     			}
     		}
-    		printf("cmd: %s \n", cmd);
-    		for (int i = 0; argv[i] != 0; i++)
-    			printf("argv: %s \n", argv[i]);
-    		printf("\n");
-    		//execvp(cmd, argv);*/
+    		bzero(temp, 20);
+    		argSize++;
+		argv[argSize] = NULL;
+		pid_t pid = fork();
+		if (pid  == 0){
+			if (execvp(cmd, argv) == -1) {
+	    			// Print error if command not found
+	    			printf("comment does not find \n");
+	    		}
+	    	}
+	    	wait(NULL);
+	    	bzero(cmd, 20);
 	}
 }
+
+
+
+
+
+/*printf("cmd: %s \n", cmd);
+    		for (int i = 0; i <= argSize; i++){
+    			printf("argv: %s \n", argv[i]);
+    			printf("size: %d \n", strlen(argv[i]));	
+    		
+    		}*/
+    		
+    				
+    				//printf("argv: %s \n", argv[argSize]);
+    		
+    		//printf("Your command: ");
+    		//printf("%s \n", command);
+    		//argv[0] = command;
+    		//printf("cmd: %s \n", cmd);
+    		//strcpy(*(argv + 0), command);
+    		//strncat(*(argv, command, 1); 
+    		//printf("%s \n", argv[0]);
